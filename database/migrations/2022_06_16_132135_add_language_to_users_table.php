a4b9701fb6373bd738e32b402be09495d054773f
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-25 16:23:33 +0200

    default languag

--- database/migrations/2022_06_16_132135_add_language_to_users_table.php
+++ database/migrations/2022_06_16_132135_add_language_to_users_table.php
@@ -14,7 +14,7 @@ class AddLanguageToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('language');
+            $table->text('language')->default('en');
         });
     }
 
