265505e5bb1bf061176bdb03e912769c4ba368af
Kevin van Herwijnen <76037700+kvherwijnen@users.noreply.github.com>
2022-06-27 08:48:49 +0200

    fixed language

--- database/migrations/2022_06_16_132135_add_language_to_users_table.php
+++ database/migrations/2022_06_16_132135_add_language_to_users_table.php
@@ -14,7 +14,7 @@ class AddLanguageToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('language')->default('en');
+            $table->text('language');
         });
     }
 
